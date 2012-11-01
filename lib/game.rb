00c44a816a6bc0864d3bd8f207679c1b38ec8367
Apprentice <apprentice@dbc26.local>
2012-11-01 13:04:19 -0700

    commiting to pull

--- lib/game.rb
+++ lib/game.rb
@@ -49,6 +49,11 @@ class Game
     @board.print_board
   end
 
+  def to_db(column,row)
+    add(@turn.id, column, row)
+
+  end
+
   def win?(column,row)
     true if @board.connect_4?(column,row) == true
     false
@@ -68,6 +73,8 @@ class Game
 
     next_turn
   end
-
-
 end
+
+game = game.new(player1, player2)
+game.turn
+game.make_move(4, 5)
