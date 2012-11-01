d858bc757b93f829456820cd124ec60519cb056f
Apprentice <apprentice@dbc26.local>
2012-10-31 23:11:58 -0700

    Different way of adding players as a suggestion

--- lib/database.rb
+++ lib/database.rb
@@ -10,6 +10,7 @@ class Database
     @db_name = db_name
     create_db(schema)
     @db = SQLite3::Database.open db_name
+    # self.db = SQLite3::Database.open db_name
   end
 
   def create_db(schema)
@@ -20,29 +21,43 @@ class Database
     @db.execute "INSERT INTO players (name, win, loss, draw) VALUES ('#{player.name}', 0, 0, 0);"
   end
 
-  def get_player(player)
-    @db.execute ("SELECT id, name, win, loss, draw FROM players where name = '#{player.name}';"){|row| return row}
-  end
+  # def get_player(player)
+  #   @db.execute ("SELECT id, name, win, loss, draw FROM players where name = '#{player.name}';"){|row| return row}
+  # end
 
-  def get_player_win_loss(player)
-    get_player(player)[2..4]
-  end
+  # def get_player_win_loss(player)
+  #   get_player(player)[2..4]
+  # end
 
-  def self.get_player_id(player)
-    database = Database.new("test_db.db")
-    database.add_player(player)
-    database.get_player(player)[0].to_s
-  end
+  # def self.get_player_id(player)
+  #   database = Database.new("test_db.db")
+  #   database.add_player(player)
+  #   database.get_player(player)[0].to_s
+  # end
 
-  def self.get_player_win(player)
-    database = Database.new("test_db.db")
-    database.add_player(player)
-    database.get_player(player)[0].to_s
-  end
+  # # def get_player_id(player)
+  # #   # database = Database.new("test_db.db")
+  # #   # database.add_player(player)
+  # #   @db.get_player(player)[0].to_s
+  # # end
 
-  def get_player_game_id(player)
-    @db.execute "SELECT MAX(id) FROM games where player1 = #{player.id}"
-  end  
+
+
+  # def self.get_player_win(player)
+  #   database = Database.new("test_db.db")
+  #   database.add_player(player)
+  #   database.get_player(player)[0].to_s
+  # end
+
+  # # def get_player_win(player)
+  # #   # database = Database.new("test_db.db")
+  # #   # database.add_player(player)
+  # #   @db.get_player(player)[0].to_s
+  # # end
+
+  # def get_player_game_id(player)
+  #   @db.execute "SELECT MAX(id) FROM games where player1 = #{player.id}"
+  # end  
 
 end
 
--- lib/player.rb
+++ lib/player.rb
@@ -2,14 +2,25 @@ require_relative 'database'
 
 class Player
 
-  attr_accessor :name, :id
+  def self.from_db(db, name)
+    attrs   = db.execute ("SELECT id, name, win, loss, draw FROM players where name = '#{name}' limit 1;")
+    pl      = Player.new(name)
+    pl.id   = attrs[0][0]
+    pl.win  = attrs[0][2]
+    pl.loss = attrs[0][3]
+    pl.draw = attrs[0][4]
+    pl
+  end
+
+  def self.to_db(db, name)
+    attrs   = db.execute "INSERT INTO players (name, win, loss, draw) VALUES ('#{name}', 0, 0, 0);"
+  end  
+
+  attr_accessor :name, :id, :win, :loss, :draw
 
 	def initialize(name)
     @name = name
-    @id = get_id
   end
-
-  
 
   def make_move
     input = gets.chomp.split(",")
@@ -27,7 +38,16 @@ class Player
 	
 end
 
-puts Player.new("Jake").id
-#print jake.id
+db      = SQLite3::Database.open "test_db.db"
+ed      = "Edward"
+newbie  = Player.to_db(db, ed)
 
-# puts joe.id
+i_am  = Player.from_db(db, ed)
+puts i_am.id
+puts i_am.name
+puts i_am.win
+puts i_am.loss
+puts i_am.draw
+
+
+
