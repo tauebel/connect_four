4f8bff9a019d9b2254b919499b8b00b92b47986b
Apprentice <apprentice@dbc26.local>
2012-11-01 21:43:28 -0700

    crated ai spec

---
+++ spec/ai_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../lib/ai'
+
+describe  AI do
+  let(:jake) { Player.new("Jake", "keemo") }
+  let(:karim) { Player.new("Karim", "bobo") }
+  let(:game) { Game.new(jake, karim) }
+  let(:ai) {AI.new(game, game.board)}
+
+  context "make_move" do
+    it "call make move on game" do
+      ai.column = 3
+      ai.row = 2
+      game.should_receive(:make_move).with(3,2)
+      ai.make_move
+    end
+
+     ai.get_possible_fields
+
+  end
+
+end
